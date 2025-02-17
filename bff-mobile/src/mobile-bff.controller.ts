482924c54900f7e50e7f2b4a413bd146b09f4eb6
ylcnfrht <ylcnfrht@gmail.com>
2025-02-17 13:36:17 +0300

    refactor: update service URLs in mobile and web BFF controllers

--- bff-mobile/src/mobile-bff.controller.ts
+++ bff-mobile/src/mobile-bff.controller.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 @Controller('mobile')
 export class MobileBFFController {
-  private readonly apiGatewayUrl = 'http://localhost:3000';
+  private readonly userServiceUrl = 'http://localhost:3001';
+  private readonly productServiceUrl = 'http://localhost:3002';
 
   @Get('users')
   async getMobileUsers() {
-    const { data } = await axios.get(`${this.apiGatewayUrl}/users`);
+    const { data } = await axios.get(`${this.userServiceUrl}/users`);
     return {
       data: data.map((user) => ({ id: user.id, name: user.name })),
     };
@@ -15,7 +16,7 @@ export class MobileBFFController {
 
   @Get('products')
   async getMobileProducts() {
-    const { data } = await axios.get(`${this.apiGatewayUrl}/products`);
+    const { data } = await axios.get(`${this.productServiceUrl}/products`);
     return {
       data: data.map((product) => ({ id: product.id, name: product.name })),
     };
--- bff-web/src/web-bff.controller.ts
+++ bff-web/src/web-bff.controller.ts
@@ -3,17 +3,17 @@ import axios from 'axios';
 
 @Controller('web')
 export class WebBFFController {
-  private readonly apiGatewayUrl = 'http://localhost:3000';
-
+  private readonly userServiceUrl = 'http://localhost:3001';
+  private readonly productServiceUrl = 'http://localhost:3002';
   @Get('users')
   async getWebUsers() {
-    const { data } = await axios.get(`${this.apiGatewayUrl}/users`);
+    const { data } = await axios.get(`${this.userServiceUrl}/users`);
     return { data };
   }
 
   @Get('products')
   async getWebProducts() {
-    const { data } = await axios.get(`${this.apiGatewayUrl}/products`);
+    const { data } = await axios.get(`${this.productServiceUrl}/products`);
     return { data };
   }
 }
